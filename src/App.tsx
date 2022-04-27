236c386df329ebe5b7d65757709c9a138a7c5d81
Manthan Kuber <manthankuber873@gmail.com>
2022-04-27 20:23:55 +0530

    React App Setup with Vite

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() => {
+    
+  
+    return () => {
+      
+    }
+  }, [])
+  
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App

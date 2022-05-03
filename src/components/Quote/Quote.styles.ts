36d9e2aa690194aaa9dce83de4d84621dbb2b098
Manthan Kuber <manthankuber873@gmail.com>
2022-05-03 17:38:41 +0530

    feat: add copy to clipboard

--- src/components/Quote/Quote.styles.ts
+++ src/components/Quote/Quote.styles.ts
@@ -1,11 +1,29 @@
 import styled from "styled-components";
+import { HiOutlineClipboardCopy, HiCheck } from "react-icons/hi";
 
 export const QuoteContainer = styled.section`
   border-left: 8px solid var(--clr-primary);
+
   font: 500 clamp(2.4rem, 2.5vw + 1rem, 3.6rem) var(--ff-raleway);
   align-self: center;
+  position: relative;
   p {
     padding-left: clamp(0.5rem, 5vw + 2rem, 10rem);
     max-width: 32ch;
   }
+`;
+
+export const CopyToClipboardIcon = styled(HiOutlineClipboardCopy)`
+  position: absolute;
+  bottom: 4%;
+  right: 0%;
+  cursor: pointer;
+  color: #787b7f;
+`;
+export const CheckIcon = styled(HiCheck)`
+  position: absolute;
+  bottom: 4%;
+  right: 0%;
+  cursor: pointer;
+  color: #787b7f;
 `;
--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -1,8 +1,11 @@
 import { motion } from "framer-motion";
+import { useRef, useState } from "react";
 import { DataProps } from "../../types";
-import { QuoteContainer } from "./Quote.styles";
+import { CheckIcon, CopyToClipboardIcon, QuoteContainer } from "./Quote.styles";
 
-const Quote = ({ data, index }:DataProps) => {
+const Quote = ({ data, index }: DataProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const quoteRef = useRef<HTMLDivElement>(null);
   const quoteAnimation = {
     hidden: { opacity: 0, x: -100 },
     show: {
@@ -12,15 +15,38 @@ const Quote = ({ data, index }:DataProps) => {
     },
   };
 
+  async function copyTextToClipboard(text: string) {
+    console.log(text);
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand("copy", true, text);
+    }
+  }
+
+  const copyOnClick = () => {
+    copyTextToClipboard(`${quoteRef.current?.innerText}`).then(() => {
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1500);
+    });
+  };
+
   return (
     <QuoteContainer
       as={motion.div}
       variants={quoteAnimation}
       initial="hidden"
       animate="show"
-      whileHover={{ scale: 1.1  }}
+      whileHover={{ scale: 1.1 }}
     >
-      <p>“{data?.content}”</p>
+      <p ref={quoteRef}>“{data?.content}”</p>
+      {document.queryCommandSupported("copy") && isCopied ? (
+        <CheckIcon />
+      ) : (
+        <CopyToClipboardIcon onClick={copyOnClick} />
+      )}
     </QuoteContainer>
   );
 };
